3727e697b518acfee6e2fa6bab44b6c661798831
logan <logan@deemos.com>
2025-02-27 07:27:52 +0000

    update: add __init__

---
+++ src/occwl/__init__.py
@@ -0,0 +1,13 @@
+from compound import Compound
+from edge import Edge
+from face import Face
+from shape import Shape
+from shell import Shell
+from solid import Solid
+from wire import Wire
+from graph import face_adjacency, vertex_adjacency
+from uvgrid import uvgrid, ugrid
+from viewer import Viewer, OffscreenRenderer
+
+from edge_data_extractor import EdgeDataExtractor
+from jupyter_viewer import JupyterViewer, JupyterRenderer, MultiSelectJupyterRenderer
